b751409c47b6db61748724560e80d523a2f2b1a3
Darius Stroman <63987827+dariusstrongman@users.noreply.github.com>
2023-10-30 11:02:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function verifyEmail() {
+    var email1 = document.getElementById("email1").value;
+    var email2 = document.getElementById("email2").value;
+
+    if (email1 !== email2) {
+        alert("Emails do not match!");
+        return false;
+    }
+}
